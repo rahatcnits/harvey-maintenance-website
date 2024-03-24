6ed56134a9c7759ddfee8a1eccc249e9796daecd
rahatcnits <rahatcnits@gmail.com>
2024-03-24 13:54:45 +0600

    completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,78 @@ navOpenBtn.addEventListener("click", () => {
 navCloseBtn.addEventListener("click", () => {
   navBox.style.right = "-100%";
 });
+
+// hero observer
+const hero = document.querySelector(".hero");
+
+const heroObserver = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("visible");
+    } else {
+      item.target.classList.remove("visible");
+    }
+  });
+});
+
+heroObserver.observe(hero);
+
+// about content observer
+const aboutContent = document.querySelector(".about_wrapper");
+
+const aboutContentObserver = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("visible");
+    } else {
+      item.target.classList.remove("visible");
+    }
+  });
+});
+
+aboutContentObserver.observe(aboutContent);
+
+// about image observer
+const aboutImg = document.querySelector(".about_img_wrapper");
+
+const aboutImgObserver = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("visible");
+    } else {
+      item.target.classList.remove("visible");
+    }
+  });
+});
+
+aboutImgObserver.observe(aboutImg);
+
+// gallery observer
+const galleryContent = document.querySelector(".gallery_content_wrapper");
+
+const galleryContentObserver = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("visible");
+    } else {
+      item.target.classList.remove("visible");
+    }
+  });
+});
+
+galleryContentObserver.observe(galleryContent);
+
+// gallery image observer
+const galleryImg = document.querySelector(".gallery_wrapper");
+
+const galleryImgObserver = new IntersectionObserver((items) => {
+  items.forEach((item) => {
+    if (item.isIntersecting) {
+      item.target.classList.add("visible");
+    } else {
+      item.target.classList.remove("visible");
+    }
+  });
+});
+
+galleryImgObserver.observe(galleryImg);
